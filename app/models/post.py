322eeec49ac4b789f0ae8a9466430898143754ab
bholeram07 <bholeram@gkmit.co>
2024-11-28 14:43:43 +0530

    feat(post-models)

---
+++ app/models/post.py
@@ -0,0 +1,16 @@
+from app.db import db
+from sqlalchemy.dialects.postgresql import UUID
+import uuid
+import datetime
+from app.models.user import User
+# users = User()
+
+class Post(db.Model):
+    __tablename__ = 'post'
+    id = db.Column(UUID(as_uuid=True),primary_key =True,default = uuid.uuid4)
+    user = db.Column(UUID(as_uuid=True), db.ForeignKey('user.id'))
+    title = db.Column(db.String(40),nullable = False)
+    content = db.Column(db.String(50),nullable = False)
+    is_deleted = db.Column(db.Boolean,nullable = False,default = False)
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
+    
