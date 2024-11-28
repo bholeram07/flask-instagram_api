13cbeb86096203e53d376cc997e8b84e52de2520
bholeram07 <bholeram@gkmit.co>
2024-11-28 14:46:47 +0530

    feat(schemas)

---
+++ app/schemas/post_schemas.py
@@ -0,0 +1,24 @@
+from marshmallow import Schema,fields,ValidationError,validates,validates_schema
+
+
+class PostSchema(Schema):
+    id  = fields.UUID(dump_only=True)
+    user = fields.UUID(dump_only=True)
+    content = fields.Str(required=True)
+    title = fields.Str(required=True)
+    @validates("title")
+    def validate_title(self,value):
+        if not value.strip():
+            raise ValidationError("Title should not be blank.")
+        if value.isdigit():
+            raise ValidationError("Title should not be purely numerical.")
+        
+    @validates("content")
+    def validate_title(self,value):
+        if not value.strip():
+            raise ValidationError("Content should not be blank.")
+       
+    
+    
+
+    
