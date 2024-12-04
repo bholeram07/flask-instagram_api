f90182964e87da99bbb5ba4dc26303b2c090877a
bholeram07 <bholeram@gkmit.co>
2024-12-04 10:51:44 +0530

    feat(comment-schemas)

---
+++ app/schemas/comment_schema.py
@@ -0,0 +1,18 @@
+from marshmallow import Schema,ValidationError , validates
+
+class Comment(Schema):
+    id = fields.UUID(dump_only = True)
+    user_id = fields.UUID(dump_only = True)
+    post_id = fields.UUID(dump_only = True)
+    Content = fields.Str(required = True)
+    
+    @validates('content')
+    def validate_content(self,value):
+        if not value.strip():
+           raise ValidationError("Title should not be blank.")
+    
+        
+        
+    
+        
+    
