6bfc070f4d92e9a4a0c6fb138d5d36d936208a8c
bholeram07 <bholeram@gkmit.co>
2024-12-06 14:33:33 +0530

    modify(user model) : user model for follower

--- app/models/user.py
+++ app/models/user.py
@@ -4,7 +4,7 @@ import uuid
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_jwt_extended import create_access_token
 import datetime
-
+from app.models.follower import Follow
 
 class User(db.Model):
     id = db.Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
@@ -20,6 +20,38 @@ class User(db.Model):
     updated_at = db.Column(
         db.DateTime, server_default=db.func.now(), onupdate=db.func.now()
     )
+    following = db.relationship('Follow',
+                               foreign_keys=[Follow.follower_id],
+                               backref=db.backref('follower', lazy='joined'),
+                               lazy='dynamic',
+                               cascade='all, delete-orphan')
+    
+    followers = db.relationship('Follow',
+                                foreign_keys=[Follow.following_id],
+                                backref=db.backref('following', lazy='joined'),
+                                lazy='dynamic',
+                                cascade='all, delete-orphan')
+    def follow(self, user):
+        if not self.is_following(user):
+            f = Follow(follower=self, following=user)
+            db.session.add(f)
+
+    def unfollow(self, user):
+        f = self.following.filter_by(following_id=user.id).first()
+        if f:
+            db.session.delete(f)
+
+    def is_following(self, user):
+        if user.id is None:
+            return False
+        return self.following.filter_by(
+            following_id=user.id).first() is not None
+
+    def is_a_follower(self, user):
+        if user.id is None:
+            return False
+        return self.followers.filter_by(
+            follower_id=user.id).first() is not None
 
     @property
     def password(self):
