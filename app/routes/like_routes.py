c511a8832122a2a1e2a9416649201096f6887df8
bholeram07 <bholeram@gkmit.co>
2024-12-10 11:43:21 +0530

    fix("like_routes")

--- app/routes/like_routes.py
+++ app/routes/like_routes.py
@@ -3,5 +3,8 @@ from flask import Blueprint
 
 like_api = Blueprint("like_api", __name__)
 like_api.add_url_rule(
-    "/posts/like/", view_func=LikeAPi.as_view("like_api"), methods=["GET", "POST",]
+    "/posts/like/", view_func=LikeAPi.as_view("like_api"), methods=["POST",]
+)
+like_api.add_url_rule(
+    "/posts/<post_id>/like/", view_func=LikeAPi.as_view("get_post_like"), methods=["GET"]
 )
