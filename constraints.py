5c662782888564c39ac35fe5277be130866e8522
bholeram07 <bholeram@gkmit.co>
2024-12-31 12:20:38 +0530

    feat:constraints file

---
+++ constraints.py
@@ -0,0 +1,12 @@
+
+from flask import request,current_app
+
+
+def get_reset_password_url(token):
+    """Dynamically generate the reset password URL."""
+    return f"{request.host_url}api/reset-password/{token}"
+
+def get_s3_file_url(new_file_key):
+    return f"{current_app.config['S3_ENDPOINT_URL']}/{current_app.config['S3_BUCKET_NAME']}/{new_file_key}"
+
+
