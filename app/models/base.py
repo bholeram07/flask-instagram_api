e6847de769f29b56445d3596009294d4c2f2c96a
bholeram07 <bholeram@gkmit.co>
2024-12-31 11:10:31 +0530

    modify:update the updated at column of the base model

--- app/models/base.py
+++ app/models/base.py
@@ -5,13 +5,13 @@ from datetime import datetime
 from app.utils.ist_time import current_time_ist
 
 class BaseModel(db.Model):
-    
+    """Base model"""
     # Indicates that this model should not be created in the database.
     __abstract__ = True
 
     id = db.Column(UUID(as_uuid=True), primary_key=True,
                    default=uuid.uuid4, unique=True, nullable=False)
     created_at = db.Column(
-        db.DateTime, default=current_time_ist, nullable=False)
-    updated_at = db.Column(db.DateTime, default=current_time_ist,
-                           onupdate=current_time_ist, nullable=False)
+        db.DateTime, default=current_time_ist)
+    updated_at = db.Column(db.DateTime, default=None,
+                           onupdate=current_time_ist)
