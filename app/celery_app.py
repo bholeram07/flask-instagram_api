427e283145feb81a54a7c50302ae0866d0f47d74
bholeram07 <bholeram@gkmit.co>
2025-01-07 12:46:14 +0530

    feat : added a celery task for deletion of post

--- app/celery_app.py
+++ app/celery_app.py
@@ -6,8 +6,8 @@ celery_app = Celery('tasks', broker='redis://localhost:6379/0',
 
 celery_app.conf.beat_schedule = {
     'delete-old-posts-every-day': {
-        'task': 'tasks.hard_delete_old_posts',
-        'schedule': crontab(hour=0, minute=0),  # Runs at 12:00 AM every day
+        'task': 'app.tasks.hard_delete_old_posts',
+        'schedule': crontab(minute='*'),  # Runs at 12:00 AM every day
     },
 }
 
